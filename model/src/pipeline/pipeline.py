5342e057bc2b9dba6e54b552e9a7fe6682894575
thedreamer67 <shaunaliting@gmail.com>
2021-06-09 01:47:59 +0800

    added code to separate sentences in data

--- model/src/pipeline/pipeline.py
+++ model/src/pipeline/pipeline.py
@@ -8,21 +8,40 @@ import numpy as np
 import re
 from sklearn.model_selection import train_test_split
 import argparse
-
+import nltk
+import gdown
+from pydrive.drive import GoogleDrive
+from pydrive.auth import GoogleAuth
 
 
 class PMCDataPipeline(object):
 
     def __init__(self):
-        pass
-        self.use_lower_only 
+        self.use_uncased = cfg['use_uncased'] # TODO not so sure about this, need to double check
     def __call__(self):
-        #your pipeline code here
-        if use_lower_only:
-            #do sth
+        # your pipeline code here
+        nltk.download(all)
+
+        # Download cleaned data from gdrive
+        raw_url = 'https://drive.google.com/uc?id=1yUVHF8Lzvi9gY3YNMjM-n7hlJR7SaG7A'
+        output = '/home/dh/Desktop/data_to_preprocess' # TODO check where to download data to
+        gdown.download(raw_url, output, quiet=False)
+        
+        with open(output) as f:
+            data = json.load(f)
+
+        # Separate the text of each section in 'text' into individual sentences
+        for ind, article in enumerate(data):
+            for i, text in enumerate(data[ind]['text']):
+                data[ind]['text'][i] = tokenizer.tokenize(text)
+        
+        if self.use_uncased: # TODO double check if this is correct
+            # lowercase everything and replace accent markers
+            # upload this data to gdrive
+            pass
         else:
-            #do else
-        pass
+            # upload unchanged data (with cases and accent markers retained) to gdrive
+            pass
     
 
     @staticmethod
