8d30a7490d4ddaff0abbb6220ec2779d193338b0
thedreamer67 <70249105+thedreamer67@users.noreply.github.com>
2021-06-18 19:27:49 +0800

    Update model.py

--- model/src/model/model.py
+++ model/src/model/model.py
@@ -99,7 +99,9 @@ class BERTModel():
             print(logits)
             print(predictions)
             print(batch['labels'])
-            metric.add_batch(predictions=predictions, references=batch['labels'])
+            references = torch.reshape(batch['labels'], (-1,))
+            print(references)
+            metric.add_batch(predictions=predictions, references=references)
         score = metric.compute()
         accuracy = score['accuracy']
         # if current epoch's accuracy is higher than the maxAccuracy recorded, replace maxAccuracy and the saved model file
