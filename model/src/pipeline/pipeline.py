a4fd4606be765dcb1f55b5866e3ead87d6308020
guannwei <guanwei58@hotmail.com>
2021-06-30 10:14:17 +0800

    Changes to pipeline for qna

--- model/src/pipeline/pipeline.py
+++ model/src/pipeline/pipeline.py
@@ -18,11 +18,16 @@ class PMCDataPipeline(object):
     def __init__(self, args):
         self.use_uncased = args.pipeline_use_uncased 
     def __call__(self):
+        self.pretrain_clean()
+        self.qna_clean()
+       
+    def pretrain_clean(self):
         nltk.download('all')
         sent_tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
         # Download cleaned data from gdrive
-        raw_url = 'https://drive.google.com/uc?id=1yUVHF8Lzvi9gY3YNMjM-n7hlJR7SaG7A'
+        #raw_url = 'https://drive.google.com/uc?id=1yUVHF8Lzvi9gY3YNMjM-n7hlJR7SaG7A' #old url
+        raw_url = 'https://drive.google.com/uc?id=1nq-5XYJ-qEe_WAZDOJuOzQGNbx9P5Wuu'
         output = 'data_to_preprocess.json' 
         gdown.download(raw_url, output, quiet=False)
         with open(output) as f:
@@ -39,7 +44,7 @@ class PMCDataPipeline(object):
                 # split into sentences
                 data[ind]['text'][i] = sent_tokenizer.tokenize(data[ind]['text'][i])
         
-                if self.use_uncased: # TODO double check if this is correct
+                if self.use_uncased: 
                     # lowercase everything and replace accent markers
                     for j, sentence in enumerate(data[ind]['text'][i]):
                         data[ind]['text'][i][j] = unidecode.unidecode(sentence.lower()) 
@@ -55,11 +60,80 @@ class PMCDataPipeline(object):
         with open(path, 'w') as outfile:
             json.dump(split_data, outfile)
 
+    def qna_clean(self):
+        # Download data
+        raw_url = 'https://drive.google.com/uc?id=1SJibr9KlCO89IQiZVMaVum9-iTb27r_s'
+        output = 'data_to_preprocess_qa.json'
+        gdown.download(raw_url, output, quiet=False)
+        with open(output) as f:
+            data = json.load(f)
+        
+        data_list = []
+        data_final = {}
+
+        # Split to train test valid
+        data_list = self.split_train_valid_test(data)
+
+        # Format data & Append to dict
+        data_final['train'] = format(data_list['train'])
+        data_final['valid'] = format(data_list['valid'])
+        data_final['test'] = format(data_list['test'])
+
+        # TODO: Check if qna needs cased and uncased
+        path = 'pipeline/uncased_qna.json' if self.use_uncased else 'pipeline/cased_qna.json'
+        with open(path, 'w') as outfile:
+            json.dump(data_final, outfile)
+
+    def format_qna(self, data_list):
+        data = []
+
+        # Append to data list
+        for group in data_list:  
+            for passage in group['paragraphs']:
+                context = passage['context']
+                for qa in passage['qas']:
+                    question = qa['question']
+                    for answer in qa['answers']:
+                        dict_cqa = {}
+
+                        # If uncased, then lowercase everything. 
+                        if self.use_uncased:
+                            context = context.lower()
+                            question = question.lower()
+                            answer = answer.lower()
+                         
+                        dict_cqa['context'] = context
+                        dict_cqa['question'] = question
+                        dict_cqa['answer'] = answer
+                        data.append(dict_cqa)
+
+        # in case index is off by 1 or 2
+        for ind, i in enumerate(data):
+            answer = data[ind]['answer']
+            context = data[ind]['context']
+            gold_text = answer['text']
+            start_idx = answer['answer_start']
+            end_idx = start_idx + len(gold_text)
+
+            if context[start_idx:end_idx] == gold_text:
+                data[ind]['answer']['answer_end'] = end_idx
+            # When the gold label is off by one character
+            elif context[start_idx-1:end_idx-1] == gold_text:
+                data[ind]['answer']['answer_start'] = start_idx - 1
+                data[ind]['answer']['answer_end'] = end_idx - 1   
+            # When the gold label is off by two characters
+            elif context[start_idx-2:end_idx-2] == gold_text:
+                data[ind]['answer']['answer_start'] = start_idx - 2
+                data[ind]['answer']['answer_end'] = end_idx - 2     
+
+        return data
+
     def split_train_valid_test(self, data):
         data_train, data_others = train_test_split(data, test_size=0.2, train_size=0.8, shuffle=False)
         data_valid, data_test = train_test_split(data_others, test_size=0.33, train_size=0.67, shuffle=False)
         split_data = {'train': data_train, 'valid': data_valid, 'test': data_test} # ratio is ~70/20/10
         return split_data
+
 
     @staticmethod
     def add_pipeline_args(parent_parser):
