68249eab05240473a7886e6c1dfcf55936046b95
thedreamer67 <shaunaliting@gmail.com>
2021-07-21 17:12:55 +0800

    minor edits

--- model/src/main.py
+++ model/src/main.py
@@ -20,26 +20,23 @@ if __name__ == '__main__':
     parser = pipeline.PMCDataPipeline.add_pipeline_args(parser)
     args = parser.parse_args()
 
-    # task = Task.init(project_name="LM Project", task_name="Fine tuning",output_uri="http://192.168.56.253:9000/minio/vsmodels/snapshots")
-    # task = Task.init(project_name="BERT", task_name="Fine tuning for domain specificity - with sampler")
+    task = Task.init(project_name="BERT", task_name="Fine tuning for domain specificity")
     # task = Task.init(project_name="BERT", task_name="test Fine tuning for QA - using pretraining ckpt (cased) early stopping, patience=3")
-    task = Task.init(project_name="BERT", task_name="testing qa")
     seed.make_deterministic(seed=random.randint(0, 10000))
     model_config_dict = task.connect_configuration(cfg,name='Model Training Parameters')
     pipeline_config_dict = task.connect_configuration(pipeline_cfg,name='Data Pipeline Parameters')
         
-    # PMC_data_pipe = pipeline.PMCDataPipeline(args)
-    # PMC_data_pipe()
+    PMC_data_pipe = pipeline.PMCDataPipeline(args)
+    PMC_data_pipe()
 
     exp = experiment.Experiment(args)
     # exp.run_experiment()
     # exp.create_torchscript_model('k=0-epoch=0.ckpt')
     # print("in main.py")
     # exp = experiment.Experiment(args)
-    # # exp = experiment.Experiment(args, task)
+    # exp = experiment.Experiment(args, task)
     pretrain_best = exp.run_experiment(task='PRETRAIN', model_startpt=None)
-    print(pretrain_best)
-    # # exp.run_experiment(task='QA', model_startpt=None)
+    # exp.run_experiment(task='QA', model_startpt=None)
     # exp.run_experiment(task='QA', model_startpt="trained_models/PRETRAIN-epoch=0.ckpt")
 
 
