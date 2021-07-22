e4df2c9a1d5ec62f8c72ca82ccbca7ce4e5c0d4a
thedreamer67 <shaunaliting@gmail.com>
2021-07-22 19:31:51 +0800

    minor edits for final code

--- model/src/main.py
+++ model/src/main.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     parser = pipeline.PMCDataPipeline.add_pipeline_args(parser)
     args = parser.parse_args()
 
-    task = Task.init(project_name="BERT", task_name="Fine tuning for domain specificity")
+    task = Task.init(project_name="BERT", task_name="Fine-tuning for domain specificity")
     # task = Task.init(project_name="BERT", task_name="test Fine tuning for QA - using pretraining ckpt (cased) early stopping, patience=3")
     seed.make_deterministic(seed=random.randint(0, 10000)) # randomize clearml seed
     model_config_dict = task.connect_configuration(cfg,name='Model Training Parameters')
